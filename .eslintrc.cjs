c5db851d5f0ca3a25d451357978436db119bce56
ChadoNihi <alexandr.nil.io@gmail.com>
2022-06-18 15:09:32 +0100

    Add pre-commit hooks w/ ESLint & Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+const ERROR = "error";
+
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: ["eslint:recommended", "plugin:jest/recommended", "prettier"],
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["jest", "svelte3"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  rules: {
+    "jest/expect-expect": [
+      ERROR,
+      {
+        assertFunctionNames: ["expect*"],
+      },
+    ],
+    eqeqeq: ERROR,
+    "no-param-reassign": ERROR,
+    "no-return-await": ERROR,
+    "no-shadow": ERROR,
+    "no-unused-expressions": [
+      ERROR,
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    "no-useless-concat": ERROR,
+    "no-useless-return": ERROR,
+    "prefer-const": ERROR,
+    "prefer-rest-params": ERROR,
+    // Code style rules:
+    camelcase: ERROR,
+    "dot-notation": ERROR,
+    "id-length": [
+      ERROR,
+      {
+        min: 3,
+        exceptions: ["a", "e", "i", "x", "y", "z", "id"],
+      },
+    ],
+    yoda: ERROR,
+  },
+  settings: {
+    jest: {
+      version: require("./package.json").devDependencies.jest,
+    },
+  },
+};
