1144941b0e136138d41339bd1eb9eb496decee42
ChadoNihi <alexandr.nil.io@gmail.com>
2022-06-19 20:34:53 +0100

    Add path aliases for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      $app: path.resolve("./src/app"),
+      $assets: path.resolve("./src/assets"),
+      $lib: path.resolve("./src/lib"),
+    },
+  },
   plugins: [svelte()],
 });
