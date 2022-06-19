569e8f7fa8685ef443d70cbf2bbf9ed3d0520ed1
ChadoNihi <alexandr.nil.io@gmail.com>
2022-06-19 20:39:19 +0100

    Implement <Header/>

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,25 @@
+<header>
+  <nav>
+    <a href="/" class="brand"><h1>TJ</h1></a>
+    <a
+      href="https://github.com/ChadoNihi/tradie-jobs-interview-task"
+      target="_blank">About</a
+    >
+  </nav>
+</header>
+
+<style>
+  .brand:hover {
+    text-decoration: none;
+  }
+  .brand h1 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  nav a:last-child {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+</style>
